bacfeac0840d748cf51244a2a663d7add5f5cb85
hunterricks <49193789+hunterricks@users.noreply.github.com>
2024-10-14 16:41:06 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import ServiceListScreen from './src/screens/ServiceListScreen';
-import ServiceDetailScreen from './src/screens/ServiceDetailScreen';
-
-const Stack = createNativeStackNavigator();
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home Services' }} />
-        <Stack.Screen name="ServiceList" component={ServiceListScreen} options={{ title: 'Services' }} />
-        <Stack.Screen name="ServiceDetail" component={ServiceDetailScreen} options={{ title: 'Service Details' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Welcome to my React Native app!</Text>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
