7a0cf6928071e82a19b8c093f13eb054056545cb
hunterricks <49193789+hunterricks@users.noreply.github.com>
2024-10-15 12:28:41 -0500

    Create home/project/git-push.js

---
+++ home/project/git-push.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const fs = require('fs').promises;
+const path = require('path');
+
+const GITHUB_TOKEN = 'YOUR_GITHUB_TOKEN_HERE';
+const REPO_OWNER = 'hunterricks';
+const REPO_NAME = 'happyhouse';
+const BRANCH = 'main';
+
+// ... rest of the script remains the same ...
